d337e466ced47c4b6b9d412e278af8b68f0de1d6
antpoo <1jiangand2@gmail.com>
2024-06-06 09:49:00 -0400

    saving progress

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # importing required dependencies
 from flask import Flask, render_template
+from flask_apscheduler import APScheduler
 from time import sleep
 import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
 
 # creating flask objects
 app = Flask(__name__)
@@ -43,9 +45,13 @@ def on_message(client, userdata, message):
 	# if the component is okay, set its status to 1
 	if msg == "OK":
 		status[topic] = 1
-	# otherwise set its status to 0
-	else:
-		status[topic] = 0
+	# if the component started, set to 0.5
+	elif msg == "START":
+		status[topic] = 0.5
+	# if the component has an error, set to -1
+	elif msg == "ERROR":
+		status[topic] = -1
+	
 
 # set its connection and message callback functions
 m_client.on_connect = on_connect
@@ -53,6 +59,20 @@ m_client.on_message = on_message
 
 # connect to broker
 m_client.connect("localhost")
+
+# set up GPIO pins for the motor
+STEP_PIN = 5
+MS1_PIN = 6
+MS2_PIN = 13
+MS3_PIN = 19
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(STEP_PIN, GPIO.OUT)
+GPIO.setup(MS1_PIN, GPIO.OUT)
+GPIO.setup(MS2_PIN, GPIO.OUT)
+GPIO.setup(MS3_PIN, GPIO.OUT)
+
 
 # method to move the conveyor
 def move_conveyor():
@@ -65,11 +85,16 @@ def check_all():
 	all_good = 1
 	# if even a single one is not good then they are not good (AND with them to check)
 	for i in range(1, 7):
-		all_good &= status[COMPONENTS[i]]
+		all_good &= status[COMPONENTS[i]] == 1
 	
 	# if everything is still good, then run the stepper motor/conveyor
 	if all_good:
-		pass
+		# set everything to 0 as it's a new cycle
+		for i in range(1, 7):
+			status[COMPONENTS[i]] = 0
+
+		# move the conveyor
+		move_conveyor()
 
 
 # main page
@@ -80,6 +105,13 @@ def index():
 
 # main app 
 if __name__ == '__main__':
+
+	# create the threading scheduler
+	scheduler = APScheduler()
+	# adding the check method to the threader
+	scheduler.add_job(func=check_all, trigger="interval", id="job", seconds=1)
+	# start the threader
+	scheduler.start()
 	
     # start the Flask app
 	app.run(host = "0.0.0.0", port=5000)
